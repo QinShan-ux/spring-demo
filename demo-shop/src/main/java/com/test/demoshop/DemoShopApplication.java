e6497d68a387a7a95f380ea0f14120240c49eda4
李楠 <li.nan@synyi.com>
2025-03-08 11:40:57 +0800

    init

---
+++ demo-shop/src/main/java/com/test/demoshop/DemoShopApplication.java
@@ -0,0 +1,14 @@
+package com.test.demoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DemoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoShopApplication.class, args);
+    }
+
+}
