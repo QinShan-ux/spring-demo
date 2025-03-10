4531fa5a3f848f86bf8b970265e31b76c3a24299
李楠 <li.nan@synyi.com>
2025-03-10 18:44:14 +0800

    添加swagger

---
+++ demo-shop/src/main/java/com/test/demoshop/config/SpringDocConfig.java
@@ -0,0 +1,36 @@
+package com.test.demoshop.config;
+
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author 李楠
+ * @Date 2025-03-10 星期一 10:11:59
+ * @Description
+ */
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("程序员API")
+                        .description("程序员的大本营")
+                        .version("v1.0.0")
+                        .license(new License()
+                                .name("许可协议")
+                                .url("https://shusheng007.top"))
+                        .contact(new Contact()
+                                .name("书生007")
+                                .email("wangben850115@gmail.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("ShuSheng007博客")
+                        .url("https://shusheng007.top"));
+        ;
+    }
+}
